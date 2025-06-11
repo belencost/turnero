e645af21ee834a20c988bce4575fe4102a4fc98c
bel <belencostantin@gmail.com>
2025-06-10 22:47:48 -0300

    arreglo

--- test_turnero.py
+++ test_turnero.py
@@ -9,7 +9,7 @@ def test_turno_correcto():
     assert turno == "CAJ-001"
 
 #Prueba incorrecta para que falle a propósito
-def test_turno_incorrecto_fallido():
-    t = Turnero()
-    turno = t.generar_turno("PERS")
-    assert turno == "PERS-005"  # Error intencional: el primer turno debería ser COM-001
+#def test_turno_incorrecto_fallido():
+    #t = Turnero()
+    #turno = t.generar_turno("PERS")
+    #assert turno == "PERS-005"  # Error intencional: el primer turno debería ser PERS-001
