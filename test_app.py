254de22f0a8c088f3a3db2794dafb6715729942d
Belén <belencostantin@gmail.com>
2025-06-12 12:21:21 -0300

    agregar pruebas a app.py

---
+++ test_app.py
@@ -0,0 +1,32 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client: #Cliente de prueba para hacer get y post
+        yield client
+
+def test_index(client): #Se simula entrar a la página y ver si el servidor responde
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_generar_turno_valido(client):
+    response = client.post("/generar_turno", json={"tipo": "CAJ"})
+    assert response.status_code == 200
+    data = response.get_json()
+    assert "turno" in data
+    assert data["turno"].startswith("CAJ-")
+
+def test_generar_turno_invalido(client):
+    response = client.post("/generar_turno", json={"tipo": "X"})
+    assert response.status_code == 400
+    data = response.get_json()
+    assert "ERROR" in data
+
+def test_estado(client):
+    client.post("/generar_turno", json={"tipo": "PERS"})
+    response = client.get("/estado")
+    assert response.status_code == 200
+    data = response.get_json()
+    assert "PERS" in data
